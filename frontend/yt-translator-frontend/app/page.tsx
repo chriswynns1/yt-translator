4eac04102fb337157ec769a2366da8398e37ac4d
chriswynns1 <98430976+chriswynns1@users.noreply.github.com>
2025-04-09 16:56:53 -0700

    more basic front end things, added navbar + material ui

--- frontend/yt-translator-frontend/app/page.tsx
+++ frontend/yt-translator-frontend/app/page.tsx
@@ -1,39 +1,24 @@
-import Image from "next/image";
+import { Button, Input } from "@material-tailwind/react";
+import { CloudUpload, NavArrowRight } from "iconoir-react";
+
 
 export default function Home() {
   return (
-    <div className="flex h-screen justify-center">
-      <div className="m-auto rounded-lg outline outline-white max-w-lg p-5">
-        <label>
-          <span className="text-2xl font-medium text-gray-700 dark:text-gray-200"> YouTube Link: </span>
-          <input
-            type="link"
-            id="link"
-            className="mt-0.5 w-full p-2 mb-2 h-10 rounded border-gray-300 shadow-sm sm:text-sm dark:border-gray-600 dark:bg-gray-900 dark:text-white"
-          />
-        </label>
+    <div className="flex justify-center mt-40">
+      
+        
 
-
-
-
-  <a
-    className="group relative inline-block w-full text-center overflow-hidden border border-cyan-600 px-4 py-2 focus:ring-3 focus:outline-hidden"
-    href="#"
-  >
-    <span
-      className="absolute inset-y-0 left-0 w-[2px] bg-cyan-600 transition-all group-hover:w-full"
-    ></span>
-  
-    <span
-      className="relative text-lg font-medium text-cyan-600 transition-colors group-hover:text-white"
-    >
-      Start translate job
-    </span>
-  </a>
-
-
-
+<form className=" mt-40 text-3xl">
+  <Input size="lg" placeholder="YouTube Link" className="p-4 mb-4"/>
+  <div className="flex flex-wrap justify-center gap-4">
+  <Button variant="gradient" className="rounded-lg">
+        Start Translation Job
+        <NavArrowRight className="ml-1 h-4 w-4 stroke-2" />
+      </Button>
       </div>
+</form>
+
+
     </div>
   );
 }
---
+++ frontend/yt-translator-frontend/components/Navbar.tsx
@@ -0,0 +1,103 @@
+"use client";
+import * as React from "react";
+import {
+  Button,
+  IconButton,
+  Typography,
+  Collapse,
+  Navbar,
+} from "@material-tailwind/react";
+import {
+  Archive,
+  Clock,
+  Menu,
+  MultiplePages,
+  ProfileCircle,
+  SelectFace3d,
+  Xmark,
+} from "iconoir-react";
+
+const LINKS = [
+  {
+    icon: Clock,
+    title: "History",
+    href: "#",
+  },
+  {
+    icon: ProfileCircle,
+    title: "Account",
+    href: "#",
+  },
+];
+
+function NavList() {
+  return (
+    <ul className="mt-4 flex flex-col gap-x-3 gap-y-1.5 lg:mt-0 lg:flex-row lg:items-center">
+      {LINKS.map(({ icon: Icon, title, href }) => (
+        <li key={title}>
+          <Typography
+            as="a"
+            href={href}
+            type="small"
+            className="flex items-center gap-x-2 p-1 hover:text-primary"
+          >
+            <Icon className="h-4 w-4" />
+            {title}
+          </Typography>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default function NavbarDemo() {
+  const [openNav, setOpenNav] = React.useState(false);
+
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false),
+    );
+  }, []);
+
+  return (
+    <Navbar className="mx-auto w-full max-w-screen-xl">
+      <div className="flex items-center">
+        <Typography
+          as="a"
+          href="#"
+          type="small"
+          className="ml-2 mr-2 block py-1 font-semibold"
+        >
+          YT-Translator
+        </Typography>
+        <hr className="ml-1 mr-1.5 hidden h-5 w-px border-l border-t-0 border-secondary-dark lg:block" />
+        <div className="hidden lg:block">
+          <NavList />
+        </div>
+        <Button size="sm" className="hidden lg:ml-auto lg:inline-block">
+          Sign In
+        </Button>
+        <IconButton
+          size="sm"
+          variant="ghost"
+          color="secondary"
+          onClick={() => setOpenNav(!openNav)}
+          className="ml-auto grid lg:hidden"
+        >
+          {openNav ? (
+            <Xmark className="h-4 w-4" />
+          ) : (
+            <Menu className="h-4 w-4" />
+          )}
+        </IconButton>
+      </div>
+      <Collapse open={openNav}>
+        <NavList />
+        <Button isFullWidth size="sm" className="mt-4">
+          Sign In
+        </Button>
+      </Collapse>
+    </Navbar>
+  );
+}
